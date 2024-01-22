775135ef74addfe447ef9e16a6d9b604db137c27
erichestella <erichestella05@gmail.com>
2024-01-23 02:09:46 +0800

    list of the given numbers

--- divisible by 5.py
+++ divisible by 5.py
@@ -2,11 +2,11 @@
 print('\nDISPLAY WHICH NUMBER IS DIVISIBLE BY 5.')
 print('Given: [10, 20, 33, 46, 55]\n')
 
+#given list of the numbers
 given_numbers = list()
-
+#given list of the numbers
 given_numbers.append(10)
 given_numbers.append(20)
 given_numbers.append(33)
 given_numbers.append(46)
 given_numbers.append(55)
-        
