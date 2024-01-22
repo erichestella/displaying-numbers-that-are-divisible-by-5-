8c118665365a04da3a50f382ae00ad45be9e2a4c
erichestella <erichestella05@gmail.com>
2024-01-23 02:06:17 +0800

    added something with the title

--- divisible by 5.py
+++ divisible by 5.py
@@ -1,2 +1,12 @@
 #adding up some title 
-print('\nDISPLAY WHICH NUMBER IS DIVISIBLE BY 5.\n')
+print('\nDISPLAY WHICH NUMBER IS DIVISIBLE BY 5.')
+print('Given: [10, 20, 33, 46, 55]\n')
+
+given_numbers = list()
+
+given_numbers.append(10)
+given_numbers.append(20)
+given_numbers.append(33)
+given_numbers.append(46)
+given_numbers.append(55)
+        
