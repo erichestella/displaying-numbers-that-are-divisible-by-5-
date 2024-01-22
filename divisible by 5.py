35784d494ee836694cf10f0f62e3e28e44f4a2ed
erichestella <erichestella05@gmail.com>
2024-01-23 02:11:55 +0800

    function to identify which is divisible by 5

--- divisible by 5.py
+++ divisible by 5.py
@@ -10,3 +10,8 @@ given_numbers.append(20)
 given_numbers.append(33)
 given_numbers.append(46)
 given_numbers.append(55)
+
+#function to identify which is divisible by 5 
+for given in given_numbers:
+    if given % 5 ==0:
+       print('Number that is divisible by 5: ', given)
